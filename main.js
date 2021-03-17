f51ec95889b73c600ff215c1308cc22a3a2fe729
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-17 15:37:35 -0400

    Adds images width to zero when clicked

--- main.js
+++ main.js
@@ -13,10 +13,23 @@ for (const dino of topDinos) {
 // create an array to target middle-dinps
 const middleDinos = document.querySelectorAll('.middle-dinos');
 
-//loop through them to add click, strikethrough
+//loop through them to add click, opacity 0;
 for (const dino of middleDinos) {
     dino.addEventListener('click', function (event) {
         const dinoClicked = event.target;
             dinoClicked.style.opacity = 0;  
+    })
+}
+
+
+// create an array to target dino images
+const dinoImages = document.querySelectorAll('#row');
+
+// loop through and hide dino images clicked
+
+for (const image of dinoImages) {
+    image.addEventListener('click', function(event) {
+        const imageClicked = event.target;
+        imageClicked.style.width = 0;
     })
 }
