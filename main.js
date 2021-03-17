b400085e1e1fed41eec8d9334243f68e19b3a8d4
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-17 15:22:36 -0400

    Adds middle dinos class and variable. Creates click even to change middle dinos opacity to 0

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// create a loop to target all of the top-dinos
+// create an array to target all of the top-dinos
 const topDinos = document.querySelectorAll('.top-dinos');
 
 
@@ -7,5 +7,16 @@ for (const dino of topDinos) {
     dino.addEventListener('click', function (event) {
         const dinoClicked = event.target;
             dinoClicked.style.textDecoration = "line-through";  
+    })
+}
+
+// create an array to target middle-dinps
+const middleDinos = document.querySelectorAll('.middle-dinos');
+
+//loop through them to add click, strikethrough
+for (const dino of middleDinos) {
+    dino.addEventListener('click', function (event) {
+        const dinoClicked = event.target;
+            dinoClicked.style.opacity = 0;  
     })
 }
