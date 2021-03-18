037794ced9dbbd4e864779f62aef12cafaaca13a
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-03-18 07:24:52 -0400

    Attempts but fails to solve meteor me button event

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ for (const dino of topDinos) {
     })
 }
 
-// create an array to target middle-dinps
+// create an array to target middle-dinos
 const middleDinos = document.querySelectorAll('.middle-dinos');
 
 //loop through them to add click, opacity 0;
@@ -23,7 +23,7 @@ for (const dino of middleDinos) {
 
 
 // create an array to target dino images
-const dinoImages = document.querySelectorAll('.row-images');
+const dinoImages = document.querySelectorAll('#row');
 
 // loop through and hide dino images clicked
 
@@ -34,24 +34,13 @@ for (const image of dinoImages) {
     })
 }
 
+//===========================================================
+
 // create an array to target all elements?
 // click event will effect each loop differently
 
 const button = document.querySelector('button');
 
-/*function destroyAll() {
-    for (const dino of topDinos) {
-        dino.style.textDecoration = "line-through";  
-    }
-
-    for (const dino of middleDinos) {
-        dino.style.opacity = 0;  
-    }
-
-    for (const image of dinoImages) {  
-        image.style.width = 0;
-    }
-}*/
 
 function destroyOne() {
     for (const dino of topDinos) {
@@ -70,13 +59,27 @@ function destroyThree(){
     }
 }
 
+function destroyAll() {
+    destroyOne();
+    destroyTwo();
+    destroyThree();
+}
+
+button.addEventListener('click', destroyAll)
+
 /*function destroyAll() {
-    destroyOne;
-    destroyTwo;
-    destroyThree;
+    for (const dino of topDinos) {
+        dino.style.textDecoration = "line-through";  
+    }
+    for (const dino of middleDinos) {
+        dino.style.opacity = 0;  
+    }
+    for (const image of dinoImages) {  
+        image.style.width = 0;
+    }
 }*/
 
-button.addEventListener('click', destroyOne)
+/*button.addEventListener('click', destroyOne)
 button.addEventListener('click', destroyTwo)
-// the bottom event listener DOES NOT WORK
-button.addEventListener('click', destroyThree)
+//the bottom event listener DOES NOT WORK
+button.addEventListener('click', destroyThree())*/
